8eb0305b12b9201d020c3714dbd2aebac8886ce8
Jerome <68373112+jeromeandrewong@users.noreply.github.com>
2023-09-23 14:01:43 +0800

    feat: implement sidebar ui (#7)
    * chore: eslint, ts and prettier configs
    
    * chore: ground work for shadcn
    
    * feat: button component
    
    * chore: add fonts
    
    * docs: icon, themig, lib, types
    
    * fix: tailwind config errors
    
    * feat: sidebar
    
    * refactor: padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex h-full items-center justify-center">
       Get started by editing&nbsp;
       <Link href={`/api/python`}>
-        <code className="font-mono font-bold">/api/index</code>
+        <code className="rounded-lg border border-border p-2 font-bold">
+          /api/index
+        </code>
       </Link>
     </main>
   );
---
+++ components/Sidebar.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { ChevronRight, List, UserCircle } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
+
+import { HamburgerToggle } from "./HamburgerToggle";
+
+import { poppins } from "@/fonts";
+import { cn } from "@/lib/utils";
+
+const sidebarLinks: SidebarNavItem[] = [
+  {
+    title: "Profile",
+    href: "/profile",
+    icon: <UserCircle />,
+  },
+  {
+    title: "Roles",
+    href: "/roles",
+    icon: <List />,
+  },
+];
+
+const Sidebar = () => {
+  const [open, setOpen] = useState(true);
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const location = usePathname();
+  return (
+    <div className="">
+      <div
+        className={`${
+          open ? "w-52" : "w-fit"
+        }  relative hidden h-screen border-r border-border px-3 pt-4 sm:block`}
+      >
+        <div
+          className="absolute -right-6 top-7 z-10 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border-2 border-border bg-border p-2"
+          onClick={() => setOpen(!open)}
+        >
+          <ChevronRight className={cn({ "rotate-180": open })} />
+        </div>
+
+        <ul className="flex flex-col gap-y-3 px-2">
+          <div>
+            <h1
+              className={cn(
+                poppins.className,
+                "pb-5  text-center text-lg font-bold text-primary",
+              )}
+            >
+              Portal
+            </h1>
+          </div>
+          {sidebarLinks.map((link, index) => (
+            <Link
+              key={index}
+              className={cn("border-l-4 border-transparent", {
+                " border-l-4 border-primary": location === link.href,
+              })}
+              href={link.href}
+            >
+              <li
+                className={cn(
+                  "flex cursor-pointer items-center gap-x-6 rounded-lg border border-transparent px-3 py-2 text-base font-normal hover:border-border",
+                  { "rounded-l-none": location === link.href },
+                )}
+              >
+                <span className="p-1">{link.icon}</span>
+                <span
+                  className={cn({ hidden: !open }, "origin-left duration-300")}
+                >
+                  {link.title}
+                </span>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      {/* Mobile Menu */}
+      <div className="pt-3">
+        <HamburgerToggle
+          mobileMenu={mobileMenu}
+          setMobileMenu={setMobileMenu}
+        />
+      </div>
+      <div className="sm:hidden">
+        <div
+          className={cn(
+            mobileMenu ? "flex" : "hidden",
+            "absolute left-6 right-6 z-50 mt-5 flex-col items-center space-y-6 self-end rounded-lg border border-border px-24 py-8 font-bold sm:w-auto",
+          )}
+        >
+          {sidebarLinks.map((link, index) => (
+            <Link
+              key={index}
+              className={cn(
+                {
+                  "text-primary underline decoration-wavy underline-offset-8":
+                    location === link.href,
+                },
+                "flex w-full items-center justify-center gap-x-1 rounded-lg py-2 pl-1 pr-3 hover:bg-secondary",
+              )}
+              href={link.href}
+              onClick={() => setMobileMenu(false)}
+            >
+              <span className="p-1.5 text-2xl">{link.icon}</span>
+              <span>{link.title}</span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
