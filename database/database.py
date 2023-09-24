d094f2ccf926ea337e2225c228fc4064885836b5
elt0nxale <eltontay1@gmail.com>
2023-09-25 01:07:58 +0800

    modify isort  config

--- database/database.py
+++ database/database.py
@@ -1,16 +1,15 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
 AWS_RDS_MYSQL_URL = os.getenv("AWS_RDS_MYSQL_URL")
 
-engine = create_engine(
-    AWS_RDS_MYSQL_URL
-)
+engine = create_engine(AWS_RDS_MYSQL_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
