a78effb5be83f4c113bef44654994f8159f03b2f
Xun Yi <xunyi.work@gmail.com>
2023-09-27 00:15:58 +0800

    Added tests for create role listing

--- api/main.py
+++ api/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import pprint
 
 app = FastAPI()
+
+class RoleListing(BaseModel):
+    role_listing_desc: str
+    role_listing_open: str
+    role_listing_close: str
+    role_listing_creator: str
 
 
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.post("/api/role_listing")
+def create_role_listing(role_details: RoleListing):
+    pprint.pprint(role_details)
+    return {"message": "Created!"}
