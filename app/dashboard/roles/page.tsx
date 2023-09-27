975f6f06e863cc3a3b17d75decc71d7fb7a74b65
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2023-09-27 18:34:43 +0800

    Table Template inserted

--- app/dashboard/roles/page.tsx
+++ app/dashboard/roles/page.tsx
@@ -1,7 +1,35 @@
-const RolesPage = () => (
-  <>
-    <div>Roles</div>
-  </>
-);
+
+import { RoleTable } from "@/components/RoleTable";
+
+const RolesPage = () => {
+  const roleData = [
+    {
+      roleId: 1,
+      roleName: "Temp Role 1",
+      roleDescription: "This is a temporary role 1",
+      roleStatus: "active",
+    },
+    {
+      roleId: 2,
+      roleName: "Temp Role 2",
+      roleDescription: "This is a temporary long description 2 This is a temporary long description 2 This is a temporary long description 2 This is a temporary long description 2 This is a temporary long description 2",
+      roleStatus: "active",
+    },
+    {
+      roleId: 3,
+      roleName: "Temp Inactive Role",
+      roleDescription: "This is a temporary inactive role",
+      roleStatus: "inactive",
+    },
+  ];
+
+  return (
+    <>
+      <div className="">Roles
+        <RoleTable roleData={roleData} />
+      </div>
+    </>
+  )
+};
 
 export default RolesPage;
