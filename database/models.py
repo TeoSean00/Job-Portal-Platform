0cb7f6f0d4330495b34afa66ed5cc1a79e44760b
elt0nxale <eltontay1@gmail.com>
2023-09-30 12:45:55 +0800

    add crud operations base layer

--- database/models.py
+++ database/models.py
@@ -1,106 +1,95 @@
-import datetime as dt
+from datetime import datetime
 
-from sqlalchemy import Column, Date, DateTime, Enum, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, DateTime, Enum, ForeignKey, Integer, String
 
-Base = declarative_base()
-
-
-class StaffDetails(Base):
-    __tablename__ = "STAFF_DETAILS"
-
-    staff_id = Column(Integer, primary_key=True)
-    fname = Column(String(50))
-    lname = Column(String(50))
-    dept = Column(String(50))
-    email = Column(String(50))
-    phone = Column(String(20))
-    biz_address = Column(String(255))
-    sys_role = Column(Enum("staff", "hr", "manager", "inactive"))
-
-
-class StaffReportingOfficer(Base):
-    __tablename__ = "STAFF_REPORTING_OFFICER"
-
-    staff_id = Column(Integer, primary_key=True)
-    RO_id = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"))
-    staff = relationship("StaffDetails")
+from database import Base
 
 
 class RoleDetails(Base):
     __tablename__ = "ROLE_DETAILS"
-
     role_id = Column(Integer, primary_key=True)
-    role_name = Column(String(50))
+    role_name = Column(String(50), nullable=False)
     role_description = Column(String(50000))
     role_status = Column(Enum("active", "inactive"))
 
 
-class StaffRoles(Base):
-    __tablename__ = "STAFF_ROLES"
-
-    staff_id = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"), primary_key=True)
-    staff_role = Column(Integer, ForeignKey("ROLE_DETAILS.role_id"), primary_key=True)
-    role_type = Column(Enum("primary", "secondary"))
-    sr_status = Column(Enum("active", "inactive"))
-
-
-class SkillDetails(Base):
-    __tablename__ = "SKILL_DETAILS"
-
-    skill_id = Column(Integer, primary_key=True)
-    skill_name = Column(String(50))
-    skill_status = Column(Enum("active", "inactive"))
-
-
-class StaffSkills(Base):
-    __tablename__ = "STAFF_SKILLS"
-
-    staff_id = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"), primary_key=True)
-    skill_id = Column(Integer, ForeignKey("SKILL_DETAILS.skill_id"), primary_key=True)
-    ss_status = Column(Enum("active", "unverified", "in-progress"))
-
-
-class RoleSkills(Base):
-    __tablename__ = "ROLE_SKILLS"
-
-    role_id = Column(Integer, ForeignKey("ROLE_DETAILS.role_id"), primary_key=True)
-    skill_id = Column(Integer, ForeignKey("SKILL_DETAILS.skill_id"), primary_key=True)
-
-
-# self defined tables for deriving additional information
+class StaffDetails(Base):
+    __tablename__ = "STAFF_DETAILS"
+    staff_id = Column(Integer, primary_key=True)
+    fname = Column(String(50), nullable=False)
+    lname = Column(String(50), nullable=False)
+    dept = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    phone = Column(String(20), nullable=False)
+    biz_address = Column(String(255), nullable=False)
+    sys_role = Column(Enum("staff", "hr", "manager"), nullable=False)
 
 
 class RoleListings(Base):
     __tablename__ = "ROLE_LISTINGS"
-
     role_listing_id = Column(Integer, primary_key=True)
-    role_id = Column(Integer, ForeignKey("ROLE_DETAILS.role_id"))
-    role_listing_desc = Column(String)
-    role_listing_source = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"))
-    role_listing_open = Column(Date)
-    role_listing_close = Column(Date)
-    role_listing_hide = Column(Date)
-    role_listing_creator = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"))
-    role_listing_ts_create = Column(DateTime, default=dt.datetime.utcnow)
+    role_id = Column(Integer, ForeignKey("ROLE_DETAILS.role_id"), nullable=False)
+    role_listing_desc = Column(String(50000))
+    role_listing_source = Column(
+        Integer, ForeignKey("STAFF_DETAILS.staff_id"), nullable=False
+    )
+    role_listing_open = Column(DateTime, nullable=False)
+    role_listing_close = Column(DateTime, nullable=False)
+    role_listing_hide = Column(DateTime)
+    role_listing_creator = Column(
+        Integer, ForeignKey("STAFF_DETAILS.staff_id"), nullable=False
+    )
+    role_listing_ts_create = Column(DateTime, nullable=False)
     role_listing_updater = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"))
-    role_listing_ts_update = Column(DateTime, default=dt.datetime.utcnow)
-
-    role = relationship("RoleDetails", backref="listings")
-    source_staff = relationship("StaffDetails", foreign_keys=[role_listing_source])
-    creator_staff = relationship("StaffDetails", foreign_keys=[role_listing_creator])
-    updater_staff = relationship("StaffDetails", foreign_keys=[role_listing_updater])
+    role_listing_ts_update = Column(DateTime)
 
 
 class RoleApplications(Base):
     __tablename__ = "ROLE_APPLICATIONS"
-
     role_app_id = Column(Integer, primary_key=True, autoincrement=True)
-    role_listing_id = Column(Integer, ForeignKey("ROLE_LISTINGS.role_listing_id"))
-    staff_id = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"))
+    role_listing_id = Column(
+        Integer, ForeignKey("ROLE_LISTINGS.role_listing_id"), nullable=False
+    )
+    staff_id = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"), nullable=False)
     role_app_status = Column(Enum("applied", "withdrawn"))
-    role_app_ts_create = Column(DateTime, default=dt.datetime.utcnow)
+    role_app_ts_create = Column(DateTime, default=datetime.utcnow)
 
-    role_listing = relationship("RoleListings", backref="applications")
-    staff = relationship("StaffDetails", backref="applications")
+
+class SkillDetails(Base):
+    __tablename__ = "SKILL_DETAILS"
+    skill_id = Column(Integer, primary_key=True)
+    skill_name = Column(String(50), nullable=False)
+    skill_status = Column(Enum("active", "inactive"), nullable=False)
+
+
+class RoleSkills(Base):
+    __tablename__ = "ROLE_SKILLS"
+    role_id = Column(Integer, ForeignKey("ROLE_DETAILS.role_id"), primary_key=True)
+    skill_id = Column(Integer, ForeignKey("SKILL_DETAILS.skill_id"), primary_key=True)
+
+
+class StaffReportingOfficer(Base):
+    __tablename__ = "STAFF_REPORTING_OFFICER"
+    staff_id = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"), primary_key=True)
+    RO_id = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"), nullable=False)
+
+
+class StaffRoles(Base):
+    __tablename__ = "STAFF_ROLES"
+    staff_id = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"), primary_key=True)
+    staff_role = Column(Integer, ForeignKey("ROLE_DETAILS.role_id"), primary_key=True)
+    role_type = Column(Enum("primary", "secondary"), nullable=False)
+    sr_status = Column(Enum("active", "inactive"), nullable=False)
+
+
+class StaffSkills(Base):
+    __tablename__ = "STAFF_SKILLS"
+    staff_id = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"), primary_key=True)
+    skill_id = Column(Integer, ForeignKey("SKILL_DETAILS.skill_id"), primary_key=True)
+    ss_status = Column(Enum("active", "unverified", "in-progress"), nullable=False)
+
+
+class ClerkStaffMatch(Base):
+    __tablename__ = "CLERK_STAFF_MATCH"
+    clerk_id = Column(Integer, primary_key=True)
+    staff_id = Column(Integer, ForeignKey("STAFF_DETAILS.staff_id"), nullable=False)
--- database/schemas.py
+++ database/schemas.py
@@ -1,48 +1,76 @@
-from pydantic import BaseModel
+from datetime import datetime
 from typing import Optional
-from datetime import date, datetime
-from enum import Enum
 
-class StaffReportingOfficerPydantic(BaseModel):
-    staff_id: int
-    RO_id: int
+from enums import (
+    RoleApplicationStatusEnum,
+    RoleTypeEnum,
+    StatusEnum,
+    SysRoleEnum,
+    VerificationStatusEnum,
+)
+from pydantic import BaseModel
 
-    class Config:
-        orm_mode = True
 
 class RoleDetailsPydantic(BaseModel):
     role_id: int
     role_name: str
     role_description: str
-    role_status: str
+    role_status: StatusEnum
 
     class Config:
         orm_mode = True
 
-class StaffRolesPydantic(BaseModel):
+
+class StaffDetailsPydantic(BaseModel):
     staff_id: int
-    staff_role: int
-    role_type: str
-    sr_status: str
+    fname: str
+    lname: str
+    dept: str
+    email: str
+    phone: str
+    biz_address: str
+    sys_role: SysRoleEnum
 
     class Config:
         orm_mode = True
+
+
+class RoleListingsPydantic(BaseModel):
+    role_listing_id: int
+    role_id: int
+    role_listing_desc: str
+    role_listing_source: int
+    role_listing_open: datetime
+    role_listing_close: datetime
+    role_listing_hide: Optional[datetime]
+    role_listing_creator: int
+    role_listing_ts_create: datetime
+    role_listing_updater: int
+    role_listing_ts_update: Optional[datetime]
+
+    class Config:
+        orm_mode = True
+
+
+class RoleApplicationsPydantic(BaseModel):
+    role_app_id: int
+    role_listing_id: int
+    staff_id: int
+    role_app_status: RoleApplicationStatusEnum
+    role_app_ts_create: datetime
+
+    class Config:
+        orm_mode = True
+
 
 class SkillDetailsPydantic(BaseModel):
     skill_id: int
     skill_name: str
-    skill_status: str
+    skill_status: StatusEnum
 
     class Config:
         orm_mode = True
 
-class StaffSkillsPydantic(BaseModel):
-    staff_id: int
-    skill_id: int
-    ss_status: str
-
-    class Config:
-        orm_mode = True
 
 class RoleSkillsPydantic(BaseModel):
     role_id: int
@@ -51,28 +79,37 @@ class RoleSkillsPydantic(BaseModel):
     class Config:
         orm_mode = True
 
-class RoleListingsPydantic(BaseModel):
-    role_listing_id: int
-    role_id: int
-    role_listing_desc: Optional[str]
-    role_listing_source: int
-    role_listing_open: date
-    role_listing_close: date
-    role_listing_hide: Optional[date]
-    role_listing_creator: int
-    role_listing_ts_create: datetime
-    role_listing_updater: int
-    role_listing_ts_update: datetime
+
+class StaffReportingOfficerPydantic(BaseModel):
+    staff_id: int
+    RO_id: int
 
     class Config:
         orm_mode = True
 
-class RoleApplicationsPydantic(BaseModel):
-    role_app_id: int
-    role_listing_id: int
+
+class StaffRolesPydantic(BaseModel):
     staff_id: int
-    role_app_status: str
-    role_app_ts_create: datetime
+    staff_role: int
+    role_type: RoleTypeEnum
+    sr_status: StatusEnum
+
+    class Config:
+        orm_mode = True
+
+
+class StaffSkillsPydantic(BaseModel):
+    staff_id: int
+    skill_id: int
+    ss_status: VerificationStatusEnum
+
+    class Config:
+        orm_mode = True
+
+
+class ClerkStaffMatch(BaseModel):
+    clerk_id: int
+    staff_id: int
 
     class Config:
         orm_mode = True
