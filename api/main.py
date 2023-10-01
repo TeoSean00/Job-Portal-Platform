5d36a29bed467a2b0b67968e05b9a09788358c87
TeoSean00 <teosean@outlook.com>
2023-10-02 01:16:04 +0800

    feat: added CORS policy for FastAPI

--- api/main.py
+++ api/main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
 from .routers import staff, roles, skills
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://is-212-spm.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Backend entry point for all routers based on their high-level functions
 app.include_router(staff.router)
 app.include_router(roles.router)
 app.include_router(skills.router)
 
-
 @app.get("/")
 def default_message():
-    return {"add /docs at end of the URL to see swagger ui documemtation"}
+    return {"add /docs at end of the URL to see swagger ui documentation"}
