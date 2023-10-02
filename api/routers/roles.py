0f0ba8fe84b64edb2e3a18b664844cac4eea9e5b
Xun Yi <xunyi.work@gmail.com>
2023-10-02 17:10:47 +0800

    Move role endpoints from main to roles

--- api/routers/roles.py
+++ api/routers/roles.py
@@ -1,4 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, HTTPException
+import datetime as dt
+
+import api.routers.common_services as common_services
+import database.services as db_services 
+from database.schemas import (
+    User,
+    RoleListingsPydantic
+)
 
 router = APIRouter(
   prefix = "/role",
@@ -9,3 +17,164 @@ router = APIRouter(
 @router.get("/")
 def default_message():
     return {"role router endpoints, refer to staff router endpoints for template!"}
+
+# =========================== Start: Role Details  ===========================
+@router.get("/role_details")
+def get_role_details(
+    user: User,
+    role_id: int = Query(None, description="Optional role_id"),
+):
+    """
+    End point that returns all the created role_listings inside the table.
+    If a role_id is specified, only return information for that role_id
+    """
+    if not common_services.authenticate_user(user, "ADMIN"):
+        raise HTTPException(status_code=401, detail="Unauthorized user!")
+    try:
+        # Return role details for all
+        if role_id == None:
+            role_details = db_services.get_all_role_details()
+            role_detail = []
+            for item in role_details.all():
+                role_detail.append(
+                    common_services.convert_sqlalchemy_object_to_dict(item)
+                )
+            return {"role_details": role_detail}
+        else:
+            role_detail = db_services.get_role_details(role_id)
+            if role_detail == None:
+                raise HTTPException(
+                    status_code=404, 
+                    detail={
+                        "message":f"Role details with id {role_id} not found!"
+                        })
+            return {"role_details": common_services.convert_sqlalchemy_object_to_dict(role_detail)}
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        # This catches all other exceptions
+        raise HTTPException(status_code=500, detail={"message":"Internal Server Error!"})
+    finally:
+        # Close DB connection if needed
+        pass
+# =========================== End: Role Details  ===========================
+
+@router.get("/role_skills")
+def get_role_skills(
+    user:User,
+    role_id: int = Query(description="Required role_id"),
+):
+    """
+    End point that takes in a role ID and returns the skills associated with that role.
+    """
+    if not common_services.authenticate_user(user, "ADMIN", "STAFF", "DIRECTOR"):
+        raise HTTPException(status_code=401, detail="Unauthorized user!")
+    try:
+        role_skills = db_services.get_role_skill(role_id)
+        if role_skills == None:
+            raise HTTPException(
+                status_code=404, 
+                detail={
+                    "message":f"Role with id {role_id} either has no skills, or does not exist!"
+                    })
+        return {"role_skills": common_services.convert_sqlalchemy_object_to_dict(role_skills)}
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        # This catches all other exceptions
+        raise HTTPException(status_code=500, detail={"message":"Internal Server Error!"})
+    finally:
+        # Close DB connection if needed
+        pass
+# =========================== End: Role Skills ===========================
+
+
+# =========================== Start: Role Listing  ===========================
+
+@router.get("/role_listing")
+def get_role_listing(
+    user: User,
+    role_listing_id: int = Query(None, description="Optional role_listing_id"),
+):
+    """
+    End point that takes in an optional role_listing_id and returns the role_listing.
+    If role_listing_id is provided, it returns information about that ID.
+    If role_listing_id is blank, it returns all role_listings.
+    """
+    if not common_services.authenticate_user(user, "ADMIN", "STAFF", "DIRECTOR"):
+        raise HTTPException(status_code=401, detail="Unauthorized user!")
+    try:
+        # Return role listing for all
+        if role_listing_id == None:
+            role_listings = db_services.get_all_role_listings()
+            role_listing = []
+            for item in role_listings.all():
+                role_listing.append(
+                    common_services.convert_sqlalchemy_object_to_dict(item)
+                )
+            return {"role_listing": role_listing}
+        # Return role listing for specific role_listing_id
+        else:
+            role_listing = db_services.get_role_listings(role_listing_id)
+            if role_listing == None:
+                raise HTTPException(
+                    status_code=404, 
+                    detail={
+                        "message":f"Role listing with id {role_listing_id} not found!"
+                        })
+            return {"role_listing": common_services.convert_sqlalchemy_object_to_dict(role_listing)}
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        # This catches all other exceptions
+        raise HTTPException(status_code=500, detail={"message":"Internal Server Error!"})
+    finally:
+        # Close DB connection if needed
+        pass
+
+@router.post("/role_listing")
+def create_role_listing(
+    user: User,
+    role_details: RoleListingsPydantic
+    ):
+    """
+    End point that takes in a role_listing, validates and creates it.
+    """
+    # Authenticate user 
+    if not common_services.authenticate_user(user, "ADMIN"):
+        raise HTTPException(status_code=401, detail="Unauthorized user!")
+    try:
+        # Validate form-details
+        role_listing_ts_create = dt.datetime.utcnow()
+        if common_services.validate_role_listing(role_details):
+            data = {
+                "role_id": 234511581, # Links to ID inside role details
+                "role_listing_desc": role_details.role_listing_desc,
+                "role_listing_source": user.user_token,
+                "role_listing_open": common_services.convert_str_to_datetime(role_details.role_listing_open),
+                "role_listing_close": common_services.convert_str_to_datetime(role_details.role_listing_close),
+                "role_listing_hide": common_services.convert_str_to_datetime(role_details.role_listing_hide),
+                "role_listing_creator": user.user_token,
+                "role_listing_ts_create": role_listing_ts_create,
+                # "role_listing_updater": None,
+                # "role_listing_ts_update": None,
+                # Set the updater and update time to the creator on first occurence
+                "role_listing_updater": user.user_token,
+                "role_listing_ts_update": role_listing_ts_create,
+            }
+            # Create role_listing
+            db_services.create_role_listing(**data)
+        else:
+            raise HTTPException(status_code=400, detail={"message":"Invalid role details!"})
+             
+        # Connect to DB and create role_listing there
+        return {"message": "Created!"}    
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        # This catches all other exceptions
+        raise HTTPException(status_code=500, detail={"message":"Internal Server Error!"})
+    finally:
+        # Close db connection 
+        pass
+# =========================== End: Role Listing  ===========================
