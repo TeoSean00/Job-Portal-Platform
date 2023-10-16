04313b59dea58145d6a0011270e222d69722c009
Jerome Wong <jeromewongjh@gmail.com>
2023-10-16 16:55:17 +0800

    fix: api routing

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
+            ? "http://127.0.0.1:8000/:path*"
             : "/api/",
       },
       {
