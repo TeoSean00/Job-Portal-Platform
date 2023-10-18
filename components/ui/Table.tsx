dfca83190a9ab8a2fb4feff361106a247dba2d00
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2023-10-18 14:37:13 +0800

    Data table fix and lint fix

--- components/ui/Table.tsx
+++ components/ui/Table.tsx
@@ -98,7 +98,10 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    className={cn(
+      "max-w-sm truncate p-4 align-middle [&:has([role=checkbox])]:pr-0",
+      className,
+    )}
     {...props}
   />
 ));
