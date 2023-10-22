fe0412960f4b300bdfa723d4fa9e155f6b5237d8
KeithLoh <keithloh99@gmail.com>
2023-10-23 01:08:59 +0800

    fix: test serverless changes

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/:path*"
-            : "/api/",
+            : "/api/routers/",
       },
       {
         source: "/docs",
