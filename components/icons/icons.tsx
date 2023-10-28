572250e3dc702a0bf39d6f9a8abb5556ea9d50fe
Jerome <68373112+jeromeandrewong@users.noreply.github.com>
2023-10-28 23:03:59 +0800

    feat: view role applicants(#41)
    * test precommit
    
    * feat: added endpoint for get_role_applicant_details
    
    * feat: add applicants page
    
    * fix: wot
    
    * refactor
    
    * fix: lint and exports
    
    * feat: psuedo skill match button
    
    ---------
    
    Co-authored-by: elt0nxale <eltontay1@gmail.com>

---
+++ components/icons/icons.tsx
@@ -0,0 +1,20 @@
+type IconProps = React.HTMLAttributes<SVGElement>;
+
+export const Icons = {
+  spinner: (props: IconProps) => (
+    <svg
+      fill="none"
+      height="24"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+    </svg>
+  ),
+};
