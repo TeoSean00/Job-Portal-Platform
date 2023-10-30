5a57670ce33db730d912282ae622bc0179dd15d8
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2023-10-30 13:18:02 +0800

    feat: apply for role (#47)
    * Dynamic Routing
    
    * Remove View Button
    
    * Role Page description
    
    * Added skills on role page and minor improvement to skills table
    
    * Data table fix and lint fix
    
    * setup precommit
    
    * Dashboard integration
    
    * Role page integration complete
    
    * Integration Done
    
    * Get all skills
    
    * Bugfix
    
    * Lint fix
    
    * Apply for Role button
    
    ---------
    
    Co-authored-by: Jerome <68373112+jeromeandrewong@users.noreply.github.com>

--- components/role-page/quickInfo.tsx
+++ components/role-page/quickInfo.tsx
@@ -2,8 +2,9 @@
 
 import type { PageData } from "../../app/dashboard/roles/[roleid]/page";
 
-import * as React from "react";
+import React, { useEffect, useContext } from "react";
 
+import { AuthContext } from "@/components/AuthProvider";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -13,12 +14,45 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { ToastAction } from "@/components/ui/toast";
+import { toast } from "@/components/ui/use-toast";
 
 interface RoleData {
   data: PageData;
 }
 export function QuickInfo(props: RoleData) {
+  const staffId = useContext(AuthContext);
   const [roleInfo, setRoleInfo] = React.useState(props.data);
+  const applyRole = () => {
+    fetch(`/api/staff/role/${staffId}/${roleInfo.roleid}`, {
+      method: "POST",
+      // headers: {
+      //   "user-token": userToken || "", // Make sure it's not undefined
+      //   role: String(userRole || ""),
+      // },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network error");
+        }
+        return res.json();
+      })
+      .then((apiData) => {
+        toast({
+          title: "Successful Appllication!",
+          description: "Please wait for HR to process your application.",
+          action: <ToastAction altText="Dismiss">Dismiss</ToastAction>,
+        });
+      })
+      .catch((err) => {
+        toast({
+          title: "Role Already Applied for!",
+          description: "Please apply for a different role",
+          action: <ToastAction altText="Dismiss">Dismiss</ToastAction>,
+        });
+        console.log("Error fetching role details:", err);
+      });
+  };
   // console.log(roleInfo);
   return (
     <Card className="w-full">
@@ -50,7 +84,7 @@ export function QuickInfo(props: RoleData) {
         </div>
       </CardContent>
       <CardFooter className="flex space-x-4">
-        <Button>Apply</Button>
+        <Button onClick={applyRole}>Apply</Button>
         <Button variant="outline">Save</Button>
       </CardFooter>
     </Card>
