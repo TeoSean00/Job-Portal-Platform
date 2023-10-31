e4840d5df668a45e5ddf36be82d9a6d446091352
Jerome <68373112+jeromeandrewong@users.noreply.github.com>
2023-10-31 14:40:44 +0800

    fix: no nested <Button/>

--- components/role-page/quickInfo.tsx
+++ components/role-page/quickInfo.tsx
@@ -109,8 +109,8 @@ export function QuickInfo(props: RoleData) {
       </CardContent>
       <CardFooter className="flex space-x-4">
         <AlertDialog>
-          <AlertDialogTrigger>
-            <Button>Apply</Button>
+          <AlertDialogTrigger className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+            Apply
           </AlertDialogTrigger>
           <AlertDialogContent>
             <AlertDialogHeader>
